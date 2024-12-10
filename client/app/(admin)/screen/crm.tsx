18f7e704190d3edfc111fbaddb743143a5feb133
NanayakkaraAR <ashinireshana@gmail.com>
2024-12-11 01:50:12 +0530

    coded profile,inventory and employee

--- client/app/(admin)/screen/crm.tsx
+++ client/app/(admin)/screen/crm.tsx
@@ -5,7 +5,10 @@ import { View, Text } from 'react-native';
 const Crm = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Crm Screen</Text>
+      <Text>
+        vouchers
+        
+      </Text>
     </View>
   );
 };
