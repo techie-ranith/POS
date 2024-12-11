4af127b53361e58087b1aaa112b7b0b5ed8158c1
NanayakkaraAR <ashinireshana@gmail.com>
2024-12-11 12:42:35 +0530

    done discount section

--- client/app/(admin)/screen/crm.tsx
+++ client/app/(admin)/screen/crm.tsx
@@ -1,14 +1,10 @@
-
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
 
 const Crm = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>
-        vouchers
-        
-      </Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>vouchers</Text>
     </View>
   );
 };
