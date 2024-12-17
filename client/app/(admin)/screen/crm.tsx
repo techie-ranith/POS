726f109fc43c9030d8025c351a990e85ffbfad06
techie-ranith <ranith2k3@gmail.com>
2024-12-17 07:02:49 +0530

    update

--- client/app/(admin)/screen/crm.tsx
+++ client/app/(admin)/screen/crm.tsx
@@ -5,9 +5,10 @@ import { View, Text } from 'react-native';
 const Crm = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Crm Screen</Text>
+      <Text>Crm Screen hello</Text>
     </View>
   );
 };
 
 export default Crm;
+ 
--- client/babel.config.js
+++ client/babel.config.js
@@ -1,10 +1,20 @@
 module.exports = function (api) {
-    api.cache(true);
-    return {
-      presets: [
-        ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-        "nativewind/babel",
-      ],
-      
-    };
+  api.cache(true);
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+      "nativewind/babel"
+    ],
+
+    plugins: [["module-resolver", {
+      root: ["./"],
+
+      alias: {
+        "@": "./",
+        "tailwind.config": "./tailwind.config.js"
+      }
+    }]]
   };
+};
