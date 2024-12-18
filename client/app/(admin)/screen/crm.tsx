5984b31d165d63cb42e04f01d58f63dd5d29fd26
techie-ranith <ranith2k3@gmail.com>
2024-12-18 10:05:52 +0530

    crm table

--- client/app/(admin)/screen/crm.tsx
+++ client/app/(admin)/screen/crm.tsx
@@ -1,66 +1,34 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import {   
-  Table,
-  TableHeader,
-  TableFooter,
-  TableBody,
-  TableHead,
-  TableData,
-  TableRow,
-  TableCaption,
- } from '@/components/ui/table';
+// import {   
+//   Table,
+//   TableHeader,
+//   TableFooter,
+//   TableBody,
+//   TableHead,
+//   TableData,
+//   TableRow,
+//   TableCaption,
+//  } from '@/components/ui/table';
 
+import Tabel from "@/components/table"
+
+ const headers = ['Name', 'Email Address', 'Phone Number'];
+const data = [
+  ['Rajesh Kumar', 'rajesh@example.com', '1234567890'],
+  ['Priya Sharma', 'priya@example.com', '1234567890'],
+  ['Ravi Patel', 'ravi@example.com', '1234567890'],
+  ['Ananya Gupta', 'ananya@example.com', '1234567890'],
+  ['Arjun Singh', 'arjun@example.com', '1234567890'],
+  ['Nisha Verma', 'nisha@example.com', '1234567890'],
+];
+ 
+const caption = 'Showing recent membership details';
+ 
 const Crm = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Crm Screen hello</Text>
-      {/* <Box className="rounded-lg overflow-hidden w-full"> */}
-    <Table className="w-full">
-      <TableHeader>
-        <TableRow>
-          <TableHead>Name</TableHead>
-          <TableHead>Email Address</TableHead>
-          <TableHead>Phone Number</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          <TableData>Rajesh Kumar</TableData>
-          <TableData >rajesh@example.com</TableData>
-          <TableData >1234567890</TableData>
-        </TableRow>
-        <TableRow>
-          <TableData>Priya Sharma</TableData>
-          <TableData >priya@example.com</TableData>
-          <TableData >1234567890</TableData>
-        </TableRow>
-        <TableRow>
-          <TableData>Ravi Patel</TableData>
-          <TableData >ravi@example.com</TableData>
-          <TableData >1234567890</TableData>
-        </TableRow>
-        <TableRow>
-          <TableData>Ananya Gupta</TableData>
-          <TableData >ananya@example.com</TableData>
-          <TableData >1234567890</TableData>
-        </TableRow>
-        <TableRow>
-          <TableData>Arjun Singh</TableData>
-          <TableData >arjun@example.com</TableData>
-          <TableData >1234567890</TableData>
-        </TableRow>
-        <TableRow>
-          <TableData>Nisha Verma</TableData>
-          <TableData >nisha@example.com</TableData>
-          <TableData >1234567890</TableData>
-        </TableRow>
-      </TableBody>
-      <TableCaption >
-        Showing recent membership details
-      </TableCaption>
-    </Table>
-    {/* </Box>   */}
+      <Tabel headers={headers} data={data} caption={caption} />
     </View>
   );
 };
--- client/app/(admin)/screen/employee.tsx
+++ client/app/(admin)/screen/employee.tsx
@@ -1,13 +1,40 @@
-
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const Employee = () => {
+// import {
+//   Table,
+//   TableHeader,
+//   TableFooter,
+//   TableBody,
+//   TableHead,
+//   TableData,
+//   TableRow,
+//   TableCaption,
+//  } from '@/components/ui/table';
+
+import Tabel from "@/components/table"
+const headers = ['Name', 'Email Address', 'Phone Number'];
+const data = [
+  ['Rajesh Kumar', 'rajesh@example.com', '1234567890'],
+  ['Priya Sharma', 'priya@example.com', '1234567890'],
+  ['Ravi Patel', 'ravi@example.com', '1234567890'],
+  ['Ananya Gupta', 'ananya@example.com', '1234567890'],
+  ['Arjun Singh', 'arjun@example.com', '1234567890'],
+  ['Nisha Verma', 'nisha@example.com', '1234567890'],
+];
+ 
+const caption = 'Showing recent membership details';
+ 
+const Crm = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Employee Screen</Text>
+      <Tabel headers={headers} data={data} caption={caption} />
     </View>
   );
 };
 
-export default Employee;
+export default Crm;
+ 
+
+
+
