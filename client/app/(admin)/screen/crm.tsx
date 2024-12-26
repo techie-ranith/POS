d2bc52f3536ac42046e200d3be69a01416ba48cf
yohanmanohara <warushayohan80@gmail.com>
2024-12-26 16:49:54 +0530

    revert

--- client/app/(admin)/screen/crm.tsx
+++ client/app/(admin)/screen/crm.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+// import {   
+//   Table,
+//   TableHeader,
+//   TableFooter,
+//   TableBody,
+//   TableHead,
+//   TableData,
+//   TableRow,
+//   TableCaption,
+//  } from '@/components/ui/table';
+
 import Tabel from "@/components/table"
 
-const caption = 'Showing recent membership details';
-const headers = ['Name', 'Email Address', 'Phone Number'];
+ const headers = ['Name', 'Email Address', 'Phone Number'];
 const data = [
   ['Rajesh Kumar', 'rajesh@example.com', '1234567890'],
   ['Priya Sharma', 'priya@example.com', '1234567890'],
@@ -12,9 +22,10 @@ const data = [
   ['Arjun Singh', 'arjun@example.com', '1234567890'],
   ['Nisha Verma', 'nisha@example.com', '1234567890'],
 ];
-
-
-const crm = () => {
+ 
+const caption = 'Showing recent membership details';
+ 
+const Crm = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Tabel headers={headers} data={data} caption={caption} />
@@ -22,4 +33,8 @@ const crm = () => {
   );
 };
 
-export default crm;
+export default Crm;
+ 
+
+
+
